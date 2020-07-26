87762dceae26832f4bc40904ad48a62bb4e7b121
SatyamGupta-py <67817984+SatyamGupta-py@users.noreply.github.com>
2020-07-26 17:23:23 +0530

    Update 1
    got it to take username input and display welcome msg

--- Display the game.py
+++ Display the game.py
@@ -1,5 +1,24 @@
 from hangMan import *
 from tkinter import *
-window = Tk()
-window.mainloop()
-window.title = "HangMan"
+game = Tk()
+
+
+game.title("HangMan")
+
+welcome = Label(game, text="Please Enter Your Name")
+welcome.grid(column=5, row=1)
+
+name = Entry(game, width=10)
+name.grid(column=5, row=3)
+
+
+def clicked():
+    msg = Label(game, text="Let's Play HangMan" + name.get())
+    msg.grid(column=5, row=4)
+
+
+name_bt = Button(game, text='Submit', width=8, command=clicked)
+name_bt.grid(column=5, row=6)
+
+
+game.mainloop()
